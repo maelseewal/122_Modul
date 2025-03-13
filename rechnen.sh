b0d0413d0c046597c3cb1f74d4bbfc62547e2edb
maelseewal <maelseewald@gmx.net>
2025-03-13 15:15:26 +0000

    rechnen.sh

---
+++ rechnen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+addition=$((1+8))          # 9
+subtraktion=$((25-9))      # 16
+divisoin=$((20/5))         # 4
+multiplikation=$((4*3))    # 12
+modulo=$((10%4))           # 2 rest 2
+potenz=$((10**2))          # 100
+
+echo $addition
+echo $subtraktion
+echo $divisoin
+echo $multiplikation
+echo $modulo
+echo $potenz
